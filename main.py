a69843823587092b500535fb47e65abc13ba48a1
PADMASHRI K.K.A <kkapadmashri@gmail.com>
2024-11-17 17:48:36 +0530

    Revert "this is the 7th version og main.py file"
    This reverts commit 8c29bf65bc0226e23bfdf09e421b0799f8b9d2c2.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("Govind")
 print("Identify wether it is krishna or narayan")
-n=input("Who is close to you? ")
+n=input("Who is close to you     ? ")
+print("Hellow")
 if n=="Friend":
     print("It is krishna")
 else:
